28d716f1fcc25a7a3e96cb4521a8ed7b4a51d76a
anhengchangyuan <188851312@qq.com>
2019-09-28 17:59:14 +0800

    perfect home list item

--- lib/widget/home_list_item.dart
+++ lib/widget/home_list_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
+// ignore: non_constant_identifier_names
 Widget HomeListItem() {
   return SliverList(
     delegate: SliverChildBuilderDelegate((context, index) {
@@ -30,8 +32,23 @@ class _ListItemState extends State<ListItem>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[],
+    return Container(
+      height: 100.0,
+      child: Column(
+        children: <Widget>[
+          // 内容视图
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+            child: _listItem(),
+          ),
+          // 分割线
+          Container(
+            margin: EdgeInsets.fromLTRB(20.0, 2.0, 20.0, 2.0),
+            color: Color(0xffeaeaea),
+            constraints: BoxConstraints.expand(height: 2.0),
+          )
+        ],
+      ),
     );
   }
 
@@ -39,5 +56,91 @@ class _ListItemState extends State<ListItem>
   void dispose() {
     _collectController?.dispose();
     super.dispose();
+  }
+}
+
+class _listItem extends StatelessWidget {
+  const _listItem({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//          mainAxisSize: MainAxisSize.max,
+//          crossAxisAlignment: CrossAxisAlignment.center,
+//          verticalDirection: VerticalDirection.down,
+          // textDirection:,
+//          textBaseline: TextBaseline.alphabetic,
+          children: <Widget>[
+            Container(
+              height: 60,
+              margin: EdgeInsets.only(left: 10),
+              child: Text(
+                '继山东编导艺考联考被曝疑似出现泄题和作弊的情况。江西编导艺考联考也被曝疑似出现泄题和作弊的情况。',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Color(0xff111111),
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Container(
+              height: 30,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text('2019-08-22'),
+                  ),
+                  Container(
+                    width: 40.0,
+                    height: 20.0,
+                    child: ButtonTheme(
+                      buttonColor: Color(0xff1C64CF),
+                      shape: StadiumBorder(),
+                      child: RaisedButton(
+                        onPressed: () => print('test'),
+                        padding: EdgeInsets.all(2.0),
+                        child: Text(
+                          '置顶',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 11.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+        SizedBox(
+          width: 15.0,
+        ),
+        Container(
+          height: 85.0,
+          width: 115.0,
+          margin: EdgeInsets.only(top: 2.0),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(5.0),
+            image: DecorationImage(
+              image: new NetworkImage('https://resources.ninghao.org/images/candy-shop.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
